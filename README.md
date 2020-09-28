1c8d361da60f9e05b54c2409cafb6f1969dddf93
rachelnicole <loveless@gmail.com>
2020-09-27 22:35:07 -0400

    removed reference to deleted file

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
-
-## Available Scripts
+## Purrybooth
 
 In the project directory, you can run:
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import Table from './Table'
 import './index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
