f5a85f495d61f23c2f10c0828487f4bff0379249
rachelnicole <loveless@gmail.com>
2020-10-14 15:07:38 -0400

    Updates

---
+++ precache-manifest.c947e6848dc371c906e9247cd7721e74.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75f34bc563bf71479b41",
+    "url": "/purrybooth/a808611a6f1a99285896.module.wasm"
+  },
+  {
+    "revision": "4e941eae4fd29ff1aa9bfe6cff4666a6",
+    "url": "/purrybooth/index.html"
+  },
+  {
+    "revision": "4647e9f7821732df641d",
+    "url": "/purrybooth/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "6bc93f8aa8f6cda43b88",
+    "url": "/purrybooth/static/css/main.a3c33fd7.chunk.css"
+  },
+  {
+    "revision": "4647e9f7821732df641d",
+    "url": "/purrybooth/static/js/2.dc1cee39.chunk.js"
+  },
+  {
+    "revision": "75f34bc563bf71479b41",
+    "url": "/purrybooth/static/js/3.e9847dea.chunk.js"
+  },
+  {
+    "revision": "6bc93f8aa8f6cda43b88",
+    "url": "/purrybooth/static/js/main.d5b768f1.chunk.js"
+  },
+  {
+    "revision": "b9c26ac193ff0878b09e",
+    "url": "/purrybooth/static/js/runtime~main.20a0a570.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/purrybooth/static/media/UploadIcon.1cedb6e9.svg"
+  }
+]);
