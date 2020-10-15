0fedee14bde8765b124d0abbe98929fa638a2476
rachelnicole <loveless@gmail.com>
2020-10-15 16:10:44 -0400

    sized canvas to fit inside window

--- src/canvas/Canvas.js
+++ src/canvas/Canvas.js
@@ -151,40 +151,35 @@ class Canvas extends React.Component {
   render() {
     return (
       <div className="decorateCanvas">
-
-        <div className="sidebar">
-          <ul>
-            <h4>Channels</h4>
-            <li id="alter_red" onClick={() => this.alterChannel(0)}>Increase Red Channel</li>
-            <li id="alter_green" onClick={() => this.alterChannel(1)}>Increase Green Channel</li>
-            <li id="alter_blue" onClick={() => this.alterChannel(2)}>Increase Blue Channel</li>
-            <li id="alter_greyscale" onClick={() => this.greyScale()}>GreyScale</li>
-            <li id="alter_threedee" onClick={() => this.threeDee()}>3D Glasses</li>
-            <li id="alter_blue" onClick={this.effectPipeline}>Inc Channel + Threshold</li>
-
-            <li id="alter_filter_oceanic" onClick={() => this.filterPhoto('oceanic')}>Oceanic</li>
-            <li id="alter_filter_islands" onClick={() => this.filterPhoto('islands')}>Islands</li>
-            <li id="alter_filter_marine" onClick={() => this.filterPhoto('marine')}>Marine</li>
-            <li id="alter_filter_seagreen" onClick={() => this.filterPhoto('seagreen')}>Seagreen</li>
-            <li id="alter_filter_flagblue" onClick={() => this.filterPhoto('flagblue')}>Flag Blue</li>
-            <li id="alter_filter_liquid" onClick={() => this.filterPhoto('liquid')}>Liquid</li>
-            <li id="alter_filter_diamante" onClick={() => this.filterPhoto('diamante')}>Diamante</li>
-            <li id="alter_filter_radio" onClick={() => this.filterPhoto('radio')}>Radio</li>
-            <li id="alter_filter_twenties" onClick={() => this.filterPhoto('twenties')}>Twenties</li>
-            <li id="alter_filter_rosetint" onClick={() => this.filterPhoto('rosetint')}>Rosetint</li>
-            <li id="alter_filter_mauve" onClick={() => this.filterPhoto('mauve')}>Mauve</li>
-            <li id="alter_filter_bluechrome" onClick={() => this.filterPhoto('bluechrome')}>Blue Monochrome</li>
-            <li id="alter_filter_vintage" onClick={() => this.filterPhoto('vintage')}>Vintage</li>
-            <li id="alter_filter_perfume" onClick={() => this.filterPhoto('perfume')}>Perfume</li>
-            <li id="alter_filter_serenity" onClick={() => this.filterPhoto('serenity')}>Serenity</li>
-
-          </ul>
-        </div>
         <section className="content">
           <h2>Image</h2>
-          <canvas ref="canvas" />
+          <canvas ref="canvas" className="decorateCanvas" />
         </section>
+        <ul>
+          <h4>Channels</h4>
+          <li id="alter_red" onClick={() => this.alterChannel(0)}>Increase Red Channel</li>
+          <li id="alter_green" onClick={() => this.alterChannel(1)}>Increase Green Channel</li>
+          <li id="alter_blue" onClick={() => this.alterChannel(2)}>Increase Blue Channel</li>
+          <li id="alter_greyscale" onClick={() => this.greyScale()}>GreyScale</li>
+          <li id="alter_threedee" onClick={() => this.threeDee()}>3D Glasses</li>
+          <li id="alter_blue" onClick={this.effectPipeline}>Inc Channel + Threshold</li>
+          <li id="alter_filter_oceanic" onClick={() => this.filterPhoto('oceanic')}>Oceanic</li>
+          <li id="alter_filter_islands" onClick={() => this.filterPhoto('islands')}>Islands</li>
+          <li id="alter_filter_marine" onClick={() => this.filterPhoto('marine')}>Marine</li>
+          <li id="alter_filter_seagreen" onClick={() => this.filterPhoto('seagreen')}>Seagreen</li>
+          <li id="alter_filter_flagblue" onClick={() => this.filterPhoto('flagblue')}>Flag Blue</li>
+          <li id="alter_filter_liquid" onClick={() => this.filterPhoto('liquid')}>Liquid</li>
+          <li id="alter_filter_diamante" onClick={() => this.filterPhoto('diamante')}>Diamante</li>
+          <li id="alter_filter_radio" onClick={() => this.filterPhoto('radio')}>Radio</li>
+          <li id="alter_filter_twenties" onClick={() => this.filterPhoto('twenties')}>Twenties</li>
+          <li id="alter_filter_rosetint" onClick={() => this.filterPhoto('rosetint')}>Rosetint</li>
+          <li id="alter_filter_mauve" onClick={() => this.filterPhoto('mauve')}>Mauve</li>
+          <li id="alter_filter_bluechrome" onClick={() => this.filterPhoto('bluechrome')}>Blue Monochrome</li>
+          <li id="alter_filter_vintage" onClick={() => this.filterPhoto('vintage')}>Vintage</li>
+          <li id="alter_filter_perfume" onClick={() => this.filterPhoto('perfume')}>Perfume</li>
+          <li id="alter_filter_serenity" onClick={() => this.filterPhoto('serenity')}>Serenity</li>
 
+        </ul>
       </div>
     )
   }
