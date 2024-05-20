09157163e081f6cab2f7e2947d60ac271cc545f9
Rachel White <loveless@gmail.com>
2024-05-20 07:45:09 -0400

    updated rum params to enable session replay features

--- src/index.js
+++ src/index.js
@@ -6,9 +6,17 @@ import App from './App'
 datadogRum.init({
   applicationId: process.env.REACT_APP_APP_ID,
   clientToken: process.env.REACT_APP_CLIENT_TOKEN,
-  site: 'datadoghq.com',
-  sampleRate: 100,
-  trackInteractions: true
+  site: 'purrybooth.com',
+  service: 'purrybooth',
+  env: 'production',
+  // Specify a version number to identify the deployed version of your application in Datadog
+  // version: '1.0.0', 
+  sessionSampleRate: 100,
+  sessionReplaySampleRate: 20,
+  trackUserInteractions: true,
+  trackResources: true,
+  trackLongTasks: true,
+  defaultPrivacyLevel: 'allow',
 });
 
 
