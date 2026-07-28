344d670789915eb0aa4ac31e8f3a3ff7b52ef9cb
Rachel White <rachel.white@datadoghq.com>
2026-07-28 11:51:43 -0400

    working through adding in page routing for components

---
+++ src/routes/mode/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	/** @type {import('./$types').PageProps} */
+	let { data } = $props();
+</script>
+
+        <p>Time to choose whether you'd like to upload one of your own photos, or use your webcam to take a new one. Either way, none of the photos are saved in any way shape or form. :)</p>
+        <div class="continue">
+        <a
+          type="button"
+          class="btn"
+          href="/upload"
+        >
+          Upload Photo
+        </a>
+
+        <button
+          type="button"
+          class="btn webcamBtn"
+          onClick={() => setStage("photo")}
+        >
+          Use Webcam
+        </button>
+        </div>
