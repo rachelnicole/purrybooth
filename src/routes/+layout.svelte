9fbaa25011cf7c07c09ff67b3e7c8167a2fb2e89
Rachel White <rachel.white@datadoghq.com>
2026-07-28 10:59:29 -0400

    copied over assets and getting basic layout rendering

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
 	import favicon from '$lib/assets/favicon.svg';
+	import '$lib/assets/app.scss';
 
 	let { children } = $props();
 </script>
@@ -8,4 +9,50 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+
+
+<div class="landing-page wrapper">
+    <div class="main-container">
+      <div class="title">
+        <div class="pull-right">
+        </div>
+        <h1><div class="icon-my-computer"></div>　プリクラ</h1>
+      </div>
+      <ul class="toolbar">
+        <li><u>A</u>bout</li>
+        <li><u>C</u>redit</li>
+        <li class="gh"><a rel="noopener noreferrer" href="https://github.com/rachelnicole/purrybooth" target="_blank"><u>G</u>itHub</a></li>
+        <li><u>H</u>elp</li>
+      </ul>
+      <div class="container-inner">
+        {@render children()}
+      </div>
+      <div class="statusbar">
+        <div class="left">made by <a rel="noopener noreferrer" href="http://www.twitter.com/ohhoe" target="_blank">ohhoe</a></div>
+        <div class="right">&nbsp;</div>
+      </div>
+    </div>
+    <div class="main-container secondary window">
+        <div class="title">
+          <div class="pull-right">
+          </div>
+          <h1><div class="icon-my-computer"></div> プリクラ</h1>
+        </div>
+        <div class="container-inner landing-container">
+        <a href="http://www.purrybooth.com" alt="landing page"><img
+              class="purrybooth-logo"
+              src="images/logo.png"
+              alt="purrybooth-logo"
+            />
+        </a>
+          
+
+        </div>
+        <div class="statusbar">
+          <div class="left">^_^</div>
+          <div class="right">&nbsp;</div>
+        </div>
+      </div>
+    </div>
+
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<img src="images/photos-splash.png"  alt="example photobooth photos" class="photo-splash">
+        <p>Purrybooth is an interactive photobooth & sticker app that is inspired by photo booths in Japan called purikura which is short for Purinto Kurabu (プリント倶楽部) or print club. They're super fun to do with friends, and you get a memory to keep afterwards. I wanted to be able to share photos with my friends while we aren't able to do the real thing in person.</p>
+        <div class="continue">
+        <button
+          type="button"
+          class="btn"
+        >
+          Let's Get Started »
+        </button>
+        </div>
